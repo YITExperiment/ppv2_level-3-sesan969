7f87ade9f015e525c481881bba9d95390615e423
sesan969 <134751249+sesan969@users.noreply.github.com>
2023-06-24 12:45:11 +0530

    Add files via upload

--- super.py
+++ super.py
@@ -1,13 +1,2 @@
-import turtle
-from itertools import cycle
 
-colors=cycle(['red','orange','yellow','green','blue','purple'])
-
-
-def draw_shape(size,angle,shift,shape):size,angle,shift,shape
-turtle.pencolor(next(colors))
-next_shape=''
-turtle.circle(50)
-next_shape='square'
-for i 1nrange(4):
 
